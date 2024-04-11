08787e88ab0446a004f262ed87245c001f36a081
Inikolay <Ibraun12345@gmail.com>
2024-04-11 15:50:08 +0300

    new

---
+++ qaAuto/src/test/java/BaseTest.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BaseTest {
+    @BeforeMethod()
+    public void openUrl() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 120000;
+        open("https://guest:welcome2qauto@qauto.forstudy.space/");
+        System.out.println("The site is open.");
+    }
+
+    @AfterMethod()
+    public void turnDown() {
+        closeWebDriver();
+        System.out.println("The site is closed.");
+    }
+}
---
+++ qaAuto/src/test/java/MyTest.java
@@ -0,0 +1,19 @@
+import io.qameta.allure.Owner;
+import main_page.ManePage;
+import org.testng.annotations.Test;
+
+public class MyTest extends BaseTest{
+
+    private String name = "Nikolay";
+    private String lastName = "Smornov";
+
+    ManePage manePage = new ManePage();
+
+    @Test
+    @Owner("Nikolay")
+    public void startTest(){
+        manePage.clickBtnLogIn()
+                .writeName(name)
+                .writeLastName(lastName);
+    }
+}
