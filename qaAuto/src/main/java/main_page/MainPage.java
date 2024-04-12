bd8aac0d1964c501282d77de2ca5e440ed8aeb49
Inikolay <Ibraun12345@gmail.com>
2024-04-12 09:27:34 +0300

    new

--- qaAuto/src/main/java/main_page/MainPage.java
+++ qaAuto/src/main/java/main_page/MainPage.java
@@ -1,5 +1,11 @@
 package main_page;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
 public class MainPage {
+
+
+    //private SelenideElement productCardNotebook = Selenide.$x("//button[@class='buy-button goods-tile__buy-button ng-star-inserted']");
 
 }
