3f0322d341c22ad83fcfa3acf2eae4c012d16d14
Inikolay <Ibraun12345@gmail.com>
2024-04-20 11:13:53 +0300

    new

--- qaAuto/src/main/java/main_page/MainPage.java
+++ qaAuto/src/main/java/main_page/MainPage.java
@@ -1,8 +1,36 @@
 package main_page;
 
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import java.time.Duration;
+
 public class MainPage {
 
-    //private SelenideElement productCardNotebook = Selenide.$x("//button[@class='buy-button goods-tile__buy-button ng-star-inserted']");
+    private SelenideElement btnSignUp = Selenide.$x("//button[@class='hero-descriptor_btn btn btn-primary']");
+    private SelenideElement inputName = Selenide.$x("//input[@id='signupName']");
+    private SelenideElement inputLastName = Selenide.$x("//input[@id='signupLastName']");
+    private SelenideElement inputEmail = Selenide.$x("//input[@id='signupEmail']");
+    private SelenideElement inputPassword = Selenide.$x("//input[@id='signupPassword']");
+    private SelenideElement inputRePassword = Selenide.$x("//input[@id='signupRepeatPassword']");
+    private SelenideElement btnRegister = Selenide.$x("//button[@class='btn btn-primary']");
+
+    public MainPage clickBtnSignUp(){
+        btnSignUp.shouldBe(Condition.visible, Duration.ofSeconds(5)).click();
+        return this;
+    }
+
+    public MainPage writeName(String name){
+        inputName.setValue(name);
+        return this;
+    }
+
+    public MainPage writeLastName(String lastName){
+        inputLastName.setValue(lastName);
+        return this;
+    }
+
 
 }
--- qaAuto/src/test/java/nikolay/TestQaAuto.java
+++ qaAuto/src/test/java/nikolay/TestQaAuto.java
@@ -1,4 +1,21 @@
 package nikolay;
 
-public class TestQaAuto {
+import basetest.BaseTest;
+import io.qameta.allure.Description;
+import io.qameta.allure.Flaky;
+import io.qameta.allure.Owner;
+import main_page.MainPage;
+import org.testng.annotations.Test;
+
+public class TestQaAuto extends BaseTest {
+
+    private MainPage mainPage = new MainPage();
+
+    @Test
+    @Owner("nikolay")
+    @Description("")
+    @Flaky
+    public void testQaAuto(){
+       mainPage.clickBtnSignUp();
+    }
 }
